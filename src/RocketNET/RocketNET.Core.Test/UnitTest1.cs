7df5ca7ba10a5d9049aac6a46068ef7a9f00c4b7
DarkSideMoon <acubens96@meta.ua>
2018-01-31 21:53:04 +0200

    Add test project and change yml file

---
+++ src/RocketNET/RocketNET.Core.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace RocketNET.Core.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(5 == 5);
+        }
+    }
+}
