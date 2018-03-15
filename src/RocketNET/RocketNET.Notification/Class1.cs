2076164259f374230a9dbcfdb1b8fa8c006f40b3
DarkSideMoon <acubens96@meta.ua>
2018-03-15 23:26:40 +0200

    Add new projects

---
+++ src/RocketNET/RocketNET.Notification/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RocketNET.Notification
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/RocketNET/RocketNET.Parser/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RocketNET.Parser
+{
+    public class Class1
+    {
+    }
+}
